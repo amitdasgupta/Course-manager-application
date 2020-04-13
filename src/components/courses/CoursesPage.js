e45043ba8a12fc79849fc445598812cf50a54551
amitdasgupta <amitdsgupta007@gmail.com>
2020-04-13 15:06:08 +0530

    added reducer and configureStore

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,35 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      courses: {
+        title: "",
+      },
+    };
+  }
+  handleChange = (event) => {
+    const courses = { ...this.state.courses, title: event.target.value };
+    this.setState({ courses });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(this.state.courses.title);
+  };
   render() {
-    return <div>courses page</div>;
+    const { handleChange, handleSubmit } = this;
+    const {
+      courses: { title },
+    } = this.state;
+    return (
+      <form onSubmit={handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input type="text" onChange={handleChange} value={title} />
+        <input type="submit" value="Submit" />
+      </form>
+    );
   }
 }
 
